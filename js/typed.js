9184969566b4b81eb2c935f6d949fdab1ed51708
Joseph Kim <josephkim1444@gmail.com>
2021-03-29 20:13:36 -0700

    Create typed.js

---
+++ js/typed.js
@@ -0,0 +1,32 @@
+// Hero Text
+var typed1 = new Typed('#typed1', {
+    strings: [
+        'I am a <strong>hard worker</strong>.',
+        'I am a <strong>foodie</strong>.',
+        'I am a <strong>team player</strong>.',
+        'I am a <strong>passionate developer</strong>.',
+        'I am a <strong>creative thinker</strong>.',
+        'I am a <strong>gamer</strong>.'
+    ],
+    typeSpeed: 50,
+    backSpeed: 25,
+    shuffle: true,
+    smartBackspace: true,
+    loop: true
+});
+
+// About Text
+var typed2 = new Typed('#typed2', {
+    strings: [
+        '`>>>` print(\'Hello! My name is Joseph.\')<br> `Hello! My name is Joseph.<br><br> >>>`^200\
+        Joseph.full_name<br> `\'Joseph Soo Kim\'<br><br> >>>`^200\
+        Joseph.college<br> `\'University of California, Irvine\'<br><br> >>>`^200\
+        Joseph.major<br> `\'Computer Science\'<br><br> >>>`^200\
+        Joseph.expected_graduation<br> `\'June 2024\'<br><br> >>>`^200\
+        Joseph.location<br> `\'Santa Clarita, CA\'<br><br> >>>`^200\
+        Joseph.emails<br> `[\'josek16@uci.edu\', \'josephkimcodes@gmail.com\']<br><br> >>>`^200\
+        Joseph.resume<br> `\'Add Link to Resume Here\'<br><br> >>>`'
+    ],
+    typeSpeed: 50,
+    backSpeed: 0
+});
